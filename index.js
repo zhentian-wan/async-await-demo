432f657ac94d16164f2dd81a4088b6da82f58745
Zhentian Wan <answer881215@gmail.com>
2017-05-03 23:41:02 +0300

    try catch

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ const BASE_URL = 'https://api.github.com/users';
 class GithubUser {
     async fetchGitHubUser(handle) {
         const response = await fetch(`${BASE_URL}/${handle}`);
-        return await response.json();
+        const body =  await response.json();
+
+        if (response.status !== 200) {
+            throw Error(body.message);
+        }
+
+        return body;
     }
 }
 
 (async () => {
     const github = new GithubUser();
-    const user = await github.fetchGitHubUser('zhentian-wan');
-    console.log(user);
+
+    try {
+        const user = await github.fetchGitHubUser('zhentian-wan');
+        console.log(user);
+    } catch(err) {
+        console.error(err);
+    }
+
+
 })();
 
